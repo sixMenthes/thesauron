b8b86de29de3099ce22668b63156d031d26db214
sixmenthes <six.menthes@gmail.com>
2024-04-05 02:12:49 +0200

    my first commit !

---
+++ april.py
@@ -0,0 +1,13 @@
+def extractContext(sentence: conll.sentence, word: token):
+    if word.index -1 > 0:
+        bigram1 = ('-1', (sentence[word.index - 1].lemma, sentence[word.index - 1].pos))
+    else:
+        bigram1 = None    
+    if word.index + 1 < sentence.length:    
+        bigram2 = ('+1', (sentence[word.index + 1].lemma, sentence[word.index + 1].pos))
+    else: 
+        bigram2 = None    
+    return bigram1, bigram2
+
+
+def 
