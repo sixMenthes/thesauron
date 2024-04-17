032bf6837654464b728a7af06a1542b5b181ae6a
sixmenthes <six.menthes@gmail.com>
2024-04-17 23:51:06 +0200

    THIS WORKS !!!!
    There's still the problem of index out of bounds
    to resolve. Now it's clunky: we're adding as many
    <d> and <f> as bow needs in the hobbiton folder
    and we're starting andmyBOW with an if

--- fellowship.py
+++ fellowship.py
@@ -1,32 +1,39 @@
 '''
 We start by importing Sam, who'll carry all the important stuff ! Sam.index gives the index in the sentence, Sam.lemma gives the lemma...
 '''
-from sam import Sam, Shire
+from hobbiton import Shire
+
+def andmyBOW(sentence: list, fellowship: dict, the_ring: set, bow= 2):
+    
+    for idx, cur_word in enumerate(sentence):
+        if (cur_word.lemma != "<d>") and (cur_word.lemma != "<f>"):
+            fellowship.setdefault(cur_word.lemma, dict())
+
+            prev_idx = 1
+            post_idx = 1
+
+            while prev_idx <= bow:
+                context = (f"-{prev_idx}", sentence[idx - prev_idx].lemma)
+                the_ring.add(context)
+                fellowship[cur_word.lemma][context] = fellowship[cur_word.lemma].get(context, 0) + 1
+                prev_idx += 1 
+
+            while post_idx <= bow:
+                context = (f"+{post_idx}", sentence[idx + post_idx].lemma)
+                the_ring.add(context)
+                fellowship[cur_word.lemma][context] = fellowship[cur_word.lemma].get(context, 0) + 1
+                post_idx += 1
+
+    return fellowship, the_ring
+
 
 with open('/Users/leo/LIL3S2/projet_tal/estrepublicain.extrait-aa.19998.outmalt', "r") as conllu:
     text = Shire(conllu.read())
 
-def andmyBOW(sentence: list, fellowship: dict, bag_of_words = 1):
-    for idx, cur_word in enumerate(sentence):
-        fellowship.setdefault(cur_word.lemma, dict())
-        for prev_word in sentence[idx - bag_of_words: idx]:
-            
 
+fellowship = dict()
+the_ring = set()
 
-
-#def andmyBOW(bag_of_words = 1):
-#    for sentence in text.sentences:
-
-
-# def andMyBow(window = 1):
-
-#     fellowship = dict()
-
-#     with open('/Users/leo/LIL3S2/projet_tal/estrepublicain.extrait-aa.19998.outmalt', "r") as conllu:
-#         text = conllu.read().split("\n").remove('')
-#         for idx, line in enumerate(text):
-#             cur_word = Sam(line)
-#             fellowship.setdefault(cur_word.lemma, dict())
-#             if (idx - window >= 0) and (idx + window <= len(text)+1):
-#                 for prev_word in text[idx - window: idx + 1]:
+for sentence in text.sentences:
+    andmyBOW(sentence, fellowship, the_ring, bow= 2)
 
