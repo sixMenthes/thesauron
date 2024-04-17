d00d185919822f162e16974fe219401143ec1139
sixmenthes <six.menthes@gmail.com>
2024-04-17 12:44:02 +0200

    blalba

--- fellowship.py
+++ fellowship.py
@@ -3,11 +3,12 @@ I believe for now we just wanna keep the lemmas, right ? This is pretty sketchy,
 to output a list of lemmas following the order of the words in the document est-republicain. each sentence is separated by an empty string. 
 '''
 
-def extractLemma(token:str):
-    if token == '':
-        return ''
-    else:
-        return token.split('\t')[2]
+def extractLemma(line:str):
+    return line.split('\t')[2]
+
+with open('/Users/leo/LIL3S2/projet_tal/estrepublicain.extrait-aa.19998.outmalt', "r") as conllu:
+    
+
 
 def conlluReader (path: str) -> list:
     #outputs a list of strings. Each member is a lemma following the order of the corpus sentences. Each sentence of the corpus is separated by an empty string.
@@ -16,26 +17,5 @@ def conlluReader (path: str) -> list:
         for token in conllu.read().split("\n"):
             post_text.append(extractLemma(token))
     return post_text 
-
-def dictBuilder (text: list):
-    #returns a dictionary of dictionaries. Keys are words, values are dicts of <keys = contexts, values = counts>
-    fellowship = dict
-    for index, word in enumerate(text):
-        if (index == 0) or (text[index-1] == ''):
-
-            fellowship.update
-            fellowship.get(word, {text[index+1]: 1})
-            fellowship.setdefault()
-
-        if (index == -1) or (text[index+1] == ''):
-            ##
-        else :
-            ###
-
-
-conllu_doc = input()
-conlluReader(conllu_doc)
-
-        
 
 
