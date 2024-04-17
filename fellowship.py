6c02ba55b683643e1779ee23e99bec98f2dbdb0b
sixmenthes <six.menthes@gmail.com>
2024-04-17 19:41:32 +0200

    inter_step

--- fellowship.py
+++ fellowship.py
@@ -1,21 +1,32 @@
 '''
 We start by importing Sam, who'll carry all the important stuff ! Sam.index gives the index in the sentence, Sam.lemma gives the lemma...
 '''
-from sam import Sam
+from sam import Sam, Shire
+
+with open('/Users/leo/LIL3S2/projet_tal/estrepublicain.extrait-aa.19998.outmalt', "r") as conllu:
+    text = Shire(conllu.read())
+
+def andmyBOW(sentence: list, fellowship: dict, bag_of_words = 1):
+    for idx, cur_word in enumerate(sentence):
+        fellowship.setdefault(cur_word.lemma, dict())
+        for prev_word in sentence[idx - bag_of_words: idx]:
+            
 
 
-def andMyAxe(window: int, cur_word: Sam, text: list, dictionary: dict):
-    for idx, word  
-    return 
-    
 
-def andMyBow(lookaround = 1):
+#def andmyBOW(bag_of_words = 1):
+#    for sentence in text.sentences:
 
-    fellowship = dict()
 
-    with open('/Users/leo/LIL3S2/projet_tal/estrepublicain.extrait-aa.19998.outmalt', "r") as conllu:
-        text = conllu.read().split("\n").remove('')
-        for idx, line in enumerate(text):
-            cur_word = Sam(line)
+# def andMyBow(window = 1):
 
+#     fellowship = dict()
+
+#     with open('/Users/leo/LIL3S2/projet_tal/estrepublicain.extrait-aa.19998.outmalt', "r") as conllu:
+#         text = conllu.read().split("\n").remove('')
+#         for idx, line in enumerate(text):
+#             cur_word = Sam(line)
+#             fellowship.setdefault(cur_word.lemma, dict())
+#             if (idx - window >= 0) and (idx + window <= len(text)+1):
+#                 for prev_word in text[idx - window: idx + 1]:
 
--- sam.py
+++ sam.py
@@ -5,3 +5,16 @@ class Sam:
         self.index = self.data[0]
         self.true = self.data[1]
         self.lemma = self.data [2]
+
+class Shire:
+
+    def __init__(self, text: str):
+
+        self.sentences = []
+        current_element = []
+        for line in text.split("\n"):
+            if line == '':
+                self.sentences.append(current_element)
+                current_element = []
+            else:
+                current_element.append(Sam(line))
